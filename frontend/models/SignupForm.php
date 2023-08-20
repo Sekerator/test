4e47102903a0cce6df6789d1cd85d1aadcfc2cba
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2023-08-21 02:39:28 +0300

    Deleting signup by mail

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -28,7 +28,6 @@ class SignupForm extends Model
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'trim'],
-            ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
             ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
@@ -54,9 +53,8 @@ class SignupForm extends Model
         $user->email = $this->email;
         $user->setPassword($this->password);
         $user->generateAuthKey();
-        $user->generateEmailVerificationToken();
 
-        return $user->save() && $this->sendEmail($user);
+        return $user->save();
     }
 
     /**
