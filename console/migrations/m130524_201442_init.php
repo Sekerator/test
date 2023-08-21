51333d1d365aed8b8acedfccc4a920080039c062
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2023-08-21 03:01:02 +0300

    Login bug fix

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -20,7 +20,7 @@ class m130524_201442_init extends Migration
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->null(),
 
-            'status' => $this->smallInteger()->notNull()->defaultValue(9),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
