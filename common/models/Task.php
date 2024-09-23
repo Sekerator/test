2a6b124838b84edb72867b96c410b0f60b4dee4d
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2024-09-23 09:42:41 +0300

    Task Model update

--- common/models/Task.php
+++ common/models/Task.php
@@ -2,7 +2,38 @@
 
 namespace common\models;
 
+/**
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property integer $due_date
+ * @property integer $status
+ * @property integer $priority
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
 class Task
 {
+    public function rules(): array
+    {
+        return [
+            [['title'], 'required'],
+            [['title', 'description'], 'string'],
+            [['due_date', 'status', 'priority', 'created_at', 'updated_at'], 'integer'],
+        ];
+    }
 
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Заголовок',
+            'description' => 'Описание',
+            'due_date' => 'Дедлайн',
+            'status' => 'Статус',
+            'priority' => 'Приоритет',
+            'created_at' => 'Создано',
+            'updated_at' => 'Обновлено',
+        ];
+    }
 }
